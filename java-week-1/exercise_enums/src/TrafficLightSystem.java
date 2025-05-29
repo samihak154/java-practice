8b528ddab125c5f7b5e20028edaa09b5d5fdf22c
samihak154 <samihak154@gmail.com>
2025-05-29 16:27:04 -0500

    Add main method with array demonstration for TrafficLight

--- java-week-1/exercise_enums/src/TrafficLightSystem.java
+++ java-week-1/exercise_enums/src/TrafficLightSystem.java
@@ -4,4 +4,13 @@ public class TrafficLightSystem {
         YELLOW,
         GREEN
     }
+
+    public static void main(String[] args) {
+        // 2. Store all values in an array
+        TrafficLight[] lights = TrafficLight.values();
+
+        int predefinedIndex = 1;
+        TrafficLight currentSignal = lights[predefinedIndex];
+        System.out.println("Traffic light signal: " + currentSignal);
+    }
 }
