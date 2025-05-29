547858698a71f24e09989aa47d20424773e970d4
samihak154 <samihak154@gmail.com>
2025-05-28 22:38:07 -0500

    Add product cost and profit calculations

--- java-week-1/exercise-shopping-cart/src/ShoppingCartApp.java
+++ java-week-1/exercise-shopping-cart/src/ShoppingCartApp.java
@@ -6,5 +6,18 @@ public class ShoppingCartApp {
         double productCost = 2.56;
         double productPrice = 4.99;
         int productQuantity = 78;
+
+        System.out.println("Welcome to the shopping cart app!");
+        System.out.println("---------------------------------");
+
+        //2. Calculate cost and show results
+        double totalCost = productCost * productQuantity;
+        System.out.printf("\nTotal cost: $%,.2f%n", totalCost);
+
+        double profitPerProduct = productPrice - productCost;
+        System.out.printf("Profit per product: $%,.2f%n", profitPerProduct);
+
+        double totalProfit = profitPerProduct * productQuantity;
+        System.out.printf("Total potential profit: $%,.2f%n", totalProfit);
     }
 }
