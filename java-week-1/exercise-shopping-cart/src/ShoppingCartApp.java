f7614689c1113e42a5c047a9ccf65415f9d8fa79
samihak154 <samihak154@gmail.com>
2025-05-28 22:35:33 -0500

    Add project file with variables declared

---
+++ java-week-1/exercise-shopping-cart/src/ShoppingCartApp.java
@@ -0,0 +1,10 @@
+public class ShoppingCartApp {
+    public static void main(String[] args) {
+        //1. Assign variables
+        int productId = 1;
+        int productCategory = 2;
+        double productCost = 2.56;
+        double productPrice = 4.99;
+        int productQuantity = 78;
+    }
+}
