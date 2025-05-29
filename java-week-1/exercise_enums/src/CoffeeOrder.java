9f23f3b592e1a7866304739efe5681867d568331
samihak154 <samihak154@gmail.com>
2025-05-29 16:24:11 -0500

    Add main method to demonstrate CoffeeSize enum usage

--- java-week-1/exercise_enums/src/CoffeeOrder.java
+++ java-week-1/exercise_enums/src/CoffeeOrder.java
@@ -5,4 +5,12 @@ public class CoffeeOrder {
         MEDIUM,
         LARGE
     }
+
+    public static void main(String[] args) {
+        // 2. Declare and initialize a CoffeeSize variable
+        CoffeeSize myCoffee = CoffeeSize.MEDIUM;
+
+        // 3. Print the selected size
+        System.out.println("Selected coffee size: " + myCoffee);
+    }
 }
