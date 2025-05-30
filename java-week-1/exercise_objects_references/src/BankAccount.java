e89124ba71b923ce5f3f8e68fbf343c0728069fb
samihak154 <samihak154@gmail.com>
2025-05-30 17:14:19 -0500

    Add new boolean variable, constructor, deposit() method, and displayStatus() method. Remove getBalance() method.

--- java-week-1/exercise_objects_references/src/BankAccount.java
+++ java-week-1/exercise_objects_references/src/BankAccount.java
@@ -1,8 +1,23 @@
 public class BankAccount {
     private String owner;
     private double balance;
+    private boolean beenModified;
 
-    public double getBalance() {
-        return balance;
+    public BankAccount(String owner, double balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        beenModified = true;
+    }
+
+    public void displayBalance() {
+        if (!beenModified) {
+            System.out.println("Initial Balance (acc1): $" + balance);
+        } else {
+            System.out.println("Updated Balance (acc1): $" + balance);
+        }
     }
 }
