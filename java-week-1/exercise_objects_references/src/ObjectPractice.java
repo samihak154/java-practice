44f5bcfe113036c5c4e8d65fd5fae12937336759
samihak154 <samihak154@gmail.com>
2025-05-30 17:39:32 -0500

    Remove comment signs from the last section and modify output so it's more organized and easier to read

--- java-week-1/exercise_objects_references/src/ObjectPractice.java
+++ java-week-1/exercise_objects_references/src/ObjectPractice.java
@@ -4,12 +4,13 @@ public class ObjectPractice {
         // Part 1: Car Dealership System
         Car car1 = new Car("Toyota", "Corolla", 2020);
         Car car2 = new Car("Ford", "Mustang", 2022);
+        System.out.println("\nPart 1");
         car1.displayInfo("Car 1: ");
         car2.displayInfo("Car 2: ");
         // Part 2: Book Library System
         Book book1 = new Book("The Hobbit", "J.R.R. Tolkien");
         Book book2 = new Book("Homo Deus", "Yuval Noah Harari");
-        System.out.println("\n");
+        System.out.println("\nPart 2");
         book1.displayStatus();
         System.out.println("Borrowing the book...");
         book1.borrowBook();
@@ -21,15 +22,16 @@ public class ObjectPractice {
         // Part 3: Shared Account Reference
         BankAccount acc1 = new BankAccount("Alice", 1000.0);
         BankAccount acc2 = acc1; // Reference copy
-        System.out.println("\n");
+        System.out.println("\nPart 3");
         acc1.displayBalance();
         System.out.println("Depositing $500 to acc2...");
         acc2.deposit(500);
         acc1.displayBalance();
-//        // Part 4: Employee Tracking
-//        new Employee("John");
-//        new Employee("Jane");
-//        new Employee("Mike");
-//        System.out.println("Total Employees: " + Employee.getTotalEmployees());
+        // Part 4: Employee Tracking
+        new Employee("John");
+        new Employee("Jane");
+        new Employee("Mike");
+        System.out.println("\nPart 4");
+        System.out.println("Total Employees: " + Employee.getTotalEmployees());
     }
 }
