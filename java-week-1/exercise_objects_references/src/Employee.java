b31f47a7fc08c1c925208074d86ac020c7eef677
samihak154 <samihak154@gmail.com>
2025-05-30 17:34:41 -0500

    Add constructor that also increments totalEmployees and getTotalEmployees() method

--- java-week-1/exercise_objects_references/src/Employee.java
+++ java-week-1/exercise_objects_references/src/Employee.java
@@ -1,4 +1,13 @@
 public class Employee {
     private String name;
     private static int totalEmployees = 0;
+
+    public Employee(String name) {
+        this.name = name;
+        totalEmployees++;
+    }
+
+    public static int getTotalEmployees() {
+        return totalEmployees;
+    }
 }
