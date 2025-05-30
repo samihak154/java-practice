e7f2c33062780ccb87ea4c9718c6c46f3c8d284a
samihak154 <samihak154@gmail.com>
2025-05-30 15:04:36 -0500

    Add variables and constructor to initialize the variables

--- java-week-1/exercise_objects_references/src/Car.java
+++ java-week-1/exercise_objects_references/src/Car.java
@@ -1,2 +1,11 @@
 public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
 }
