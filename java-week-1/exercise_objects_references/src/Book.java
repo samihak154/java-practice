e3c2e3261d0a1386d937b8e41778c856932986df
samihak154 <samihak154@gmail.com>
2025-05-30 16:44:10 -0500

    Add constructor, borrowBook() method, and displayStatus() method

--- java-week-1/exercise_objects_references/src/Book.java
+++ java-week-1/exercise_objects_references/src/Book.java
@@ -2,4 +2,18 @@ public class Book {
     private String title;
     private String author;
     private boolean isAvailable;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public void borrowBook() {
+        isAvailable = false;
+    }
+
+    public void displayStatus() {
+        System.out.println("Book: " + title + " by " + author + " (Available: " + isAvailable + ")");
+    }
 }
