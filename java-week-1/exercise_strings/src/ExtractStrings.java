5d3546e42d7f23e23d09f4adbc7c87fe8165fcad
samihak154 <samihak154@gmail.com>
2025-05-31 16:55:46 -0500

    Declare a string, then extract parts of the string

--- java-week-1/exercise_strings/src/ExtractStrings.java
+++ java-week-1/exercise_strings/src/ExtractStrings.java
@@ -1,2 +1,36 @@
+/*PART 2: EXTRACTING PARTS OF STRING
+Task: Use substring() to Extract Words
+1. Declare a string:
+a. String sentence = "Learning Java is fun!";
+2. Extract and print:
+    ○ "Learning" (characters 0 to 8).
+    ○ "Java" (characters 9 to 13).
+    ○ "fun!" (last word using a single argument with substring()).
+
+Example Output:
+First word: Learning
+Second word: Java
+Last word: fun!*/
 public class ExtractStrings {
+    public static void main(String[] args) {
+        // 1. Declare a string
+        String sentence = "Learning Java is fun!";
+
+        // 2. Extract parts of the string
+        String firstWord = sentence.substring(0, 8);
+        String secondWord = sentence.substring(9, 13);
+        String lastWord = sentence.substring(17, 21);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
