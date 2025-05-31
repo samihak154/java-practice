77ae795f349528ea43c9487d269a06ce078771f6
samihak154 <samihak154@gmail.com>
2025-05-31 17:04:36 -0500

    Declare string of comma separated values, then use split() to break them into an array

--- java-week-1/exercise_strings/src/SplitStrings.java
+++ java-week-1/exercise_strings/src/SplitStrings.java
@@ -1,2 +1,22 @@
+/*PART 3: SPLITTING STRINGS
+Task: Split a String and Process its Parts
+1. Declare a string:
+    a. String csvData = "apple,banana,grape,orange";
+2. Use split() to break it into an array.
+3. Loop through the array and print each item.
+
+Example Output:
+Fruit 1: apple
+Fruit 2: banana
+Fruit 3: grape
+Fruit 4: orange*/
 public class SplitStrings {
+    public static void main(String[] args) {
+        // 1. Declare a string
+        String csvData = "apple,banana,grape,orange";
+
+        // 2. Use split() to break it into an array
+        String[] fruits = csvData.split(",");
+    }
 }
+
