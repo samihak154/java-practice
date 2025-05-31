ec94aa9a9137d5686a3cb781006e0081cd5e138f
samihak154 <samihak154@gmail.com>
2025-05-31 17:18:41 -0500

    Declare a string sentence, then replace parts of the string with new words and underscores, then print the updated string

--- java-week-1/exercise_strings/src/ReplaceCharacters.java
+++ java-week-1/exercise_strings/src/ReplaceCharacters.java
@@ -1,2 +1,11 @@
 public class ReplaceCharacters {
+    public static void main(String[] args) {
+        // 1. Declare a string
+        String sentence = "The quick brown fox.";
+        // 2. Replace "quick" with "slow", and replace all spaces with underscores (_)
+        String newSentence = sentence.replace("quick","slow");
+        String finalSentence = newSentence.replace(" ","_");
+        // 3. Print
+        System.out.println("Modified sentence: " + finalSentence);
+    }
 }
