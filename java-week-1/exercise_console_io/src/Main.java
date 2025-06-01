661b728e3b73a6752d998f1ad2be036617dea0ff
samihak154 <samihak154@gmail.com>
2025-05-31 22:13:41 -0500

    Add code to display a formatted receipt

--- java-week-1/exercise_console_io/src/Main.java
+++ java-week-1/exercise_console_io/src/Main.java
@@ -7,17 +7,17 @@ public class Main {
         // 1. Ask the user for their name
         System.out.print("What is your name? \n");
         String name = io.nextLine();
-        System.out.println("\nHello, " + name + "! Let's get started with your order.\n");
+        System.out.println("\nHello, " + name + "! Let's get started with your order.");
 
         // 2. Get product details: Product name, quantity, unit price
-        System.out.print("What product would you like to purchase? \n");
+        System.out.print("\nWhat product would you like to purchase? \n");
         String productName = io.nextLine();
 
-        System.out.print("How many would you like?\n");
+        System.out.print("\nHow many would you like?\n");
         String inputQuantity = io.nextLine();
         double quantity = Double.parseDouble(inputQuantity);
 
-        System.out.print("What is the unit price?\n");
+        System.out.print("\nWhat is the unit price?\n");
         String inputUnitPrice = io.nextLine();
         double unitPrice = Double.parseDouble(inputUnitPrice);
 
@@ -27,5 +27,16 @@ public class Main {
         double productGrandTotal = productSubtotal + productTax;
 
         // 4. Print a formatted receipt
+        System.out.println("\nOrder Summary");
+        System.out.println("---------------------------------");
+        System.out.println("Product: \t\t" + productName);
+        System.out.printf("Quantity: \t\t%.0f%n", quantity);
+        System.out.printf("Price: \t\t\t$%.2f%n", unitPrice);
+        System.out.println("---------------------------------");
+        System.out.printf("Subtotal: \t\t$%.2f%n", productSubtotal);
+        System.out.printf("Tax: \t\t\t$%.2f%n", productTax);
+        System.out.printf("Grand Total: \t$%.2f%n", productGrandTotal);
+        System.out.println("---------------------------------");
+        System.out.println("Thank you for your order, " + name + "!");
     }
 }
