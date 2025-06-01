8e134009d55bdab555cb6162abd0ec0bfe8bedf2
samihak154 <samihak154@gmail.com>
2025-05-31 19:18:24 -0500

    Add code to get user's name, output introducotry greeting, and get product details, such as, product name, quantity, and unit price.

--- java-week-1/exercise_console_io/src/Main.java
+++ java-week-1/exercise_console_io/src/Main.java
@@ -5,8 +5,19 @@ public class Main {
         Scanner io = new Scanner(System.in);
 
         // 1. Ask the user for their name
+        System.out.print("What is your name? \n");
+        String name = io.nextLine();
+        System.out.println("\nHello, " + name + "! Let's get started with your order.\n");
 
         // 2. Get product details: Product name, quantity, unit price
+        System.out.print("What product would you like to purchase? \n");
+        String productName = io.nextLine();
+
+        System.out.print("How many would you like?\n");
+        String quantity = io.nextLine();
+
+        System.out.print("What is the unit price?\n");
+        String unitPrice = io.nextLine();
 
         // 3. Calculate total price
 //            ○ Subtotal = Quantity * Unit Price
