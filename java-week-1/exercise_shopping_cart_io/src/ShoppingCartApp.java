83605c3200ee2e0010b2f5ddceb86d37312878ea
samihak154 <samihak154@gmail.com>
2025-06-01 01:22:30 -0500

    Add code to collect user input and display said input and modify the order of the code for better organization

--- java-week-1/exercise_shopping_cart_io/src/ShoppingCartApp.java
+++ java-week-1/exercise_shopping_cart_io/src/ShoppingCartApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ShoppingCartApp {
     public static void main(String[] args) {
         //1. Assign variables
@@ -9,20 +11,39 @@ public class ShoppingCartApp {
         String businessName = "Pumba's Pet Supplies";
         String businessContactInfo = "123-456-7890";
         String productDescription = "Squeezable Treat Variety Pack";
+        Scanner io = new Scanner(System.in);
 
-        //2. Calculate cost and show results
+        //2. Prompts
+        System.out.println("Welcome to the shopping cart app!");
+        System.out.println("---------------------------------");
+        System.out.print("\nAre you tax-exempt? \n");
+        String taxExempt = io.nextLine();
+        System.out.print("\nShipping? \n");
+        String shipping = io.nextLine();
+        System.out.print("\nOrder Quantity? \n");
+        String quantity = io.nextLine();
+        double orderQuantity = Double.parseDouble(quantity);
+        System.out.print("\nPromo code for free shipping? \n");
+        String promoCode = io.nextLine();
+
+        //3. Calculate cost and show results
+        productQuantity = (int) orderQuantity;
         double totalCost = productCost * productQuantity;
         double profitPerProduct = productPrice - productCost;
         double totalProfit = profitPerProduct * productQuantity;
-
-        //3. Print
-        System.out.println("Welcome to the shopping cart app!");
-        System.out.println("---------------------------------");
+        
+        //4. Print
+        System.out.println("\n---------------------------------------");
+        System.out.println("Details");
         System.out.println("Product: " + productDescription);
+        System.out.println("Tax-exempt: " + taxExempt);
+        System.out.println("Shipping: " + shipping);
+        System.out.println("Order quantity: " + orderQuantity);
+        System.out.println("Promo code: " + promoCode);
         System.out.printf("Total cost: $%,.2f%n", totalCost);
         System.out.printf("Profit per product: $%,.2f%n", profitPerProduct);
         System.out.printf("Total potential profit: $%,.2f%n", totalProfit);
-        System.out.println("\n---------------------------------");
+        System.out.println("\n---------------------------------------");
         System.out.println("Business Info");
         System.out.println("Business name: " + businessName);
         System.out.println("Contact Number: " + businessContactInfo);
