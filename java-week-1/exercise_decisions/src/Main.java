48eee9118bea0f25b15857e89be6e51b112e3a46
samihak154 <samihak154@gmail.com>
2025-06-01 16:48:36 -0500

    Remove Main.java file

--- java-week-1/exercise_decisions/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-    // 1. Welcome the player and explain the scenario
-        //    - Print an introduction message
-        //    - Ask if they want to enter the cave
-        //     - Get user input (yes/no)
-    }
-}
