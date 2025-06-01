98ba19e8d4cbf0a821695455fd372fd4ec84818e
samihak154 <samihak154@gmail.com>
2025-05-31 19:05:35 -0500

    Import the Scanner class from java.utils package and set up program flow with comments

--- java-week-1/exercise_console_io/src/Main.java
+++ java-week-1/exercise_console_io/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner io = new Scanner(System.in);
 
+        // 1. Ask the user for their name
+
+        // 2. Get product details: Product name, quantity, unit price
+
+        // 3. Calculate total price
+//            ○ Subtotal = Quantity * Unit Price
+//            ○ Tax (7%) = Subtotal * 0.07
+//            ○ Grand Total = Subtotal + Tax
+
+        // 4. Print a formatted receipt
     }
 }
