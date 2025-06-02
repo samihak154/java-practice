61d75dd3a113e2c8a85bb63c9677dde051cab7b2
samihak154 <samihak154@gmail.com>
2025-06-01 23:10:55 -0500

    Add code to declare and assign values to an array, access and modify the array, and find the length of the array

--- java-week-1/exercise_arrays/src/BasicArrayOperations.java
+++ java-week-1/exercise_arrays/src/BasicArrayOperations.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class BasicArrayOperations {
+    public static void main(String[] args) {
+        // 1.  Declare & Assign an Array
+        String[] cities = {"Austin", "San Antonio", "Houston", "Dallas", "El Paso"};
+        for (int i = 0; i < cities.length; i++) {
+            System.out.println(cities[i]);
+        }
+        // 2. Access & Modify Elements
+        cities[2] = "Fort Worth";
+        System.out.println("\nUpdated Cities:");
+        System.out.println("---------------");
+        for (int i = 0; i < cities.length; i++) {
+            System.out.println(cities[i]);
+        }
+        // 3. Find the Length of an Array
+        System.out.println("\nTotal cities: " + cities.length);
+    }
 }
