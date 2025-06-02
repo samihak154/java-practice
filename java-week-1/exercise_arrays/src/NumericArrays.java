70d190a660f480763e20f4c6c89f61ce9486c3e0
samihak154 <samihak154@gmail.com>
2025-06-02 00:00:33 -0500

    Add code to find the sum of numbers in an array, find the min and max, and calculate the average

--- java-week-1/exercise_arrays/src/NumericArrays.java
+++ java-week-1/exercise_arrays/src/NumericArrays.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class NumericArrays {
+    public static void main(String[] args) {
+        // 7. Sum of Numbers in an Array
+        double[] testScores = {89.7, 96.0, 93.5, 88.0, 82.9};
+        double sum = 0.0;
+        for (int i = 0; i < testScores.length; i++) {
+            sum += testScores[i];
+        }
+        System.out.println("Sum of all test scores: " + sum);
+        // 8. Find the Maximum & Minimum
+        double highest = testScores[0];
+        double lowest = testScores[0];
+        for (int i = 0; i < testScores.length; i++) {
+            if(testScores[i] > highest) {
+                highest = testScores[i];
+            }
+        }
+        for (int i = 0; i < testScores.length; i++) {
+            if(testScores[i] < lowest) {
+                lowest = testScores[i];
+            }
+        }
+        System.out.println("Highest score: " + highest);
+        System.out.println("Lowest score: " + lowest);
+        // 9. Calculate the Average
+        double average = sum / testScores.length;
+        System.out.printf("Average score: %.1f%n", average);
+
+    }
 }
