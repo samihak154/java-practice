65e6eeb43f3573cf114e08728d06018f23ada182
samihak154 <samihak154@gmail.com>
2025-06-01 23:41:20 -0500

    Modify code to print the elements of the array manually without a for loop

--- java-week-1/exercise_arrays/src/BasicArrayOperations.java
+++ java-week-1/exercise_arrays/src/BasicArrayOperations.java
@@ -2,16 +2,20 @@ public class BasicArrayOperations {
     public static void main(String[] args) {
         // 1.  Declare & Assign an Array
         String[] cities = {"Austin", "San Antonio", "Houston", "Dallas", "El Paso"};
-        for (int i = 0; i < cities.length; i++) {
-            System.out.println(cities[i]);
-        }
+        System.out.println("City 1: " + cities[0]);
+        System.out.println("City 1: " + cities[1]);
+        System.out.println("City 1: " + cities[2]);
+        System.out.println("City 1: " + cities[3]);
+        System.out.println("City 1: " + cities[4]);
         // 2. Access & Modify Elements
         cities[2] = "Fort Worth";
         System.out.println("\nUpdated Cities:");
         System.out.println("---------------");
-        for (int i = 0; i < cities.length; i++) {
-            System.out.println(cities[i]);
-        }
+        System.out.println("City 1: " + cities[0]);
+        System.out.println("City 1: " + cities[1]);
+        System.out.println("City 1: " + cities[2]);
+        System.out.println("City 1: " + cities[3]);
+        System.out.println("City 1: " + cities[4]);
         // 3. Find the Length of an Array
         System.out.println("\nTotal cities: " + cities.length);
     }
