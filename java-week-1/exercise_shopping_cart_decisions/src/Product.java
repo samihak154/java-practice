0cb8d533ee6f22df3c3530fbb25603b7437e7175
samihak154 <samihak154@gmail.com>
2025-06-01 20:21:48 -0500

    Move product data from the ShoppingCartApp class to this one

--- java-week-1/exercise_shopping_cart_decisions/src/Product.java
+++ java-week-1/exercise_shopping_cart_decisions/src/Product.java
@@ -1,2 +1,19 @@
 public class Product {
+    private int id;
+    private int category;
+    private double cost;
+    private double price;
+    private String description;
+
+    public Product(int id, int category, double cost, double price, String description) {
+        this.id = id;
+        this.category = category;
+        this.cost = cost;
+        this.price = price;
+        this.description = description;
+    }
+
+    public double getCost() { return cost; }
+    public double getPrice() { return price; }
+    public String getDescription() { return description; }
 }
