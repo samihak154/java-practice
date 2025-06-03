e144afc316f353bf29696266e80f1d88517ed7ff
samihak154 <samihak154@gmail.com>
2025-06-03 15:51:52 -0500

    Add proper messages for the choices (in variables instead of directly in the loop) and add validation check

--- java-week-1/lab_text_adventure_loops/src/AdventureGame.java
+++ java-week-1/lab_text_adventure_loops/src/AdventureGame.java
@@ -41,51 +41,77 @@ public class AdventureGame {
         boolean visitedMoonHall = false;
         boolean hasSunKeyHalf = false;
         boolean hasMoonKeyHalf = false;
-
+        String mainMenu = "\nYou find yourself in the grand chamber of an ancient pyramid. The are 3 passages." +
+                "\nWhich one do you choose?" +
+                "\n1. Chamber of the Sun God" +
+                "\n2. Hall of the Moon Priestess" +
+                "\n3. Chamber of the Eclipse" +
+                "\n4. Quit game" +
+                "\nYou choose: ";
+        String sunFirstMessage = """
+        \nYou enter a circular chamber with walls adorned in intricate golden details.
+        A strange glow illuminates half of a ceremonial key on a pedestal. 
+        You take the half with you.
+        """;
+        String sunSecondMessage = "\nYou find yourself back in the golden chamber. You've already got the key.";
+        String moonFirstMessage = """
+        \nYou step into a hallway lined with beautiful murals of lunar cycles. 
+        At the end, a glittering crescent-shaped altar holds half of a ceremonial key.
+        You take the half with you.
+        """;
+        String moonSecondMessage = "\nYou find yourself back in the hallway. You've already got the key.";
+        String eclipseMessage = """
+        \nThe two key halves combine together to form a key that perfectly matches the keyhole on the stone slab. 
+        Once you enter the key, the slab slowly moves aside, revealing a portal to a beautiful lake surrounded by
+        mountains and frozen in time. You explore for what feels like weeks, yet return to find not a second has passed,
+        your body untouched by age.
+        You decide to leave the pyramid with the key, knowing you'll want to return.
+        """;
+        String eclipseLockedMessage = """
+        \nThe chamber is sealed by a stone slab with the engraving: 
+        "Only when light and shadow unite shall the path open."
+        Right under the engraving there appears to be a distinct keyhole waiting to be filled. 
+        You need to look for all parts of the key!
+        """;
+        String gameOverMessage = "\nGame over. Thank you for playing!";
 
         Scanner io = new Scanner(System.in);
 
-        System.out.println("Welcome to the Text Adventure Game!");
+        System.out.println("Welcome to the Text Adventure Game!\n");
 
         while (true) {
-            System.out.print("\nYou find yourself in the grand chamber of an ancient pyramid. The are 3 passages." +
-                            "\nWhich one do you choose?" +
-                            "\n1. Chamber of the Sun God" +
-                            "\n2. Hall of the Moon Priestess" +
-                            "\n3. Chamber of the Eclipse" +
-                            "\n4. Quit game" +
-                            "\nYou chose: ");
+            System.out.print(mainMenu);
             String choice = io.nextLine();
+
             if (choice.equals("3")) {
                 if (!hasSunKeyHalf && !hasMoonKeyHalf) {
-                    System.out.println("Sorry! You have to unite the Sun and Moon chambers by collecting " +
-                                       "the keys first!");
+                    System.out.println(eclipseLockedMessage);
                 } else {
-                    System.out.println("Unlock message for Eclipse chamber");
-                    System.out.println("Thank you for playing the game!");
+                    System.out.println(eclipseMessage);
+                    System.out.println(gameOverMessage);
                     break;
                 }
             } else if (choice.equals("1")) {
                 if (!visitedSunChamber) {
-                    System.out.println("Show first message for Sun chamber");
+                    System.out.println(sunFirstMessage);
                     visitedSunChamber = true;
                     hasSunKeyHalf = true;
                 } else {
-                    System.out.println("Show second message for Sun chamber");
-                    break;
+                    System.out.println(sunSecondMessage);
                 }
             } else if (choice.equals("2")) {
                 if (!visitedMoonHall) {
-                    System.out.println("Show first message Moon chamber");
+                    System.out.println(moonFirstMessage);
                     visitedMoonHall = true;
                     hasMoonKeyHalf = true;
                 } else {
-                    System.out.println("Show second message for Moon chamber");
-                    break;
+                    System.out.println(moonSecondMessage);
                 }
-            } else {
-                System.out.println("You've chosen to exit the game.");
+            } else if (choice.equals("4")){
+                System.out.println("\nYou chose to leave the pyramid, its secrets untouched.");
                 break;
+            } else {
+                System.out.println("\nInvalid choice, please choose 1, 2, 3 or 4");
             }
         }
     }
