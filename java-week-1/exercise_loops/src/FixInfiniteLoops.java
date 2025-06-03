1fd5b1fcb8a7fb6e21571ec1fb9fcaf09a26660e
samihak154 <samihak154@gmail.com>
2025-06-02 19:29:58 -0500

    Fix an infinite loop to stop at a specific number and add another loop with even number check to prevent an infinite loop

--- java-week-1/exercise_loops/src/FixInfiniteLoops.java
+++ java-week-1/exercise_loops/src/FixInfiniteLoops.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FixInfiniteLoops {
+    public static void main(String[] args) {
+        // Fix the Infinite Loop
+        System.out.println("Infinite loop fixed: ");
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+
+        // Even Number Checker (Preventing Infinite Loops)
+        Scanner io = new Scanner(System.in);
+        int userNumber;
+        while (true) {  // Infinite loop until break
+            System.out.print("\nEnter an even number: ");
+            userNumber = Integer.parseInt(io.nextLine());
+            if (userNumber % 2 == 0) {
+                break;
+            }
+            System.out.println("That's an odd number, please enter an even number!");
+        }
+        System.out.println("Exiting program...");
+    }
 }
+
