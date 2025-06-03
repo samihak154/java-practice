7b26a598d2a9fa3e10fc6c00c0aa91d2b3e01630
samihak154 <samihak154@gmail.com>
2025-06-03 14:34:27 -0500

    Declare variables, get user input and add core game loop (final descriptions not added yet).

--- java-week-1/lab_text_adventure_loops/src/AdventureGame.java
+++ java-week-1/lab_text_adventure_loops/src/AdventureGame.java
@@ -15,7 +15,7 @@
  *    - Vault (Room 3): Locked until both key halves are combined
  *
  * VARIABLES:
- *    - boolean visitedSunChamber, visitedMoonHall, visitedEclipseChamber
+ *    - boolean visitedSunChamber, visitedMoonHall
  *    - boolean hasSunKeyHalf, hasMoonKeyHalf
  *
  * PROGRAM FLOW:
@@ -33,6 +33,60 @@
  *               - Unlocked: Show unlocked message, then exit game
  *    end loop
  */
+import java.util.Scanner;
 
 public class AdventureGame {
+    public static void main(String[] args) {
+        boolean visitedSunChamber = false;
+        boolean visitedMoonHall = false;
+        boolean hasSunKeyHalf = false;
+        boolean hasMoonKeyHalf = false;
+
+
+        Scanner io = new Scanner(System.in);
+
+        System.out.println("Welcome to the Text Adventure Game!");
+
+        while (true) {
+            System.out.print("\nYou find yourself in the grand chamber of an ancient pyramid. The are 3 passages." +
+                            "\nWhich one do you choose?" +
+                            "\n1. Chamber of the Sun God" +
+                            "\n2. Hall of the Moon Priestess" +
+                            "\n3. Chamber of the Eclipse" +
+                            "\n4. Quit game" +
+                            "\nYou chose: ");
+            String choice = io.nextLine();
+            if (choice.equals("3")) {
+                if (!hasSunKeyHalf && !hasMoonKeyHalf) {
+                    System.out.println("Sorry! You have to unite the Sun and Moon chambers by collecting " +
+                                       "the keys first!");
+                } else {
+                    System.out.println("Unlock message for Eclipse chamber");
+                    System.out.println("Thank you for playing the game!");
+                    break;
+                }
+            } else if (choice.equals("1")) {
+                if (!visitedSunChamber) {
+                    System.out.println("Show first message for Sun chamber");
+                    visitedSunChamber = true;
+                    hasSunKeyHalf = true;
+                } else {
+                    System.out.println("Show second message for Sun chamber");
+                    break;
+                }
+            } else if (choice.equals("2")) {
+                if (!visitedMoonHall) {
+                    System.out.println("Show first message Moon chamber");
+                    visitedMoonHall = true;
+                    hasMoonKeyHalf = true;
+                } else {
+                    System.out.println("Show second message for Moon chamber");
+                    break;
+                }
+            } else {
+                System.out.println("You've chosen to exit the game.");
+                break;
+            }
+        }
+    }
 }
