8e0627511e103b78bcbc99c12e97d1dcfb58844c
samihak154 <samihak154@gmail.com>
2025-06-03 18:54:07 -0500

    Add methods to display choices and prompt user for string and int, call methods and print details in the main method, delete file with old name and update with new name (ShoppingCartApp).

--- java-week-1/exercise_shopping_cart_methods/src/ShoppingCartApp.java
+++ java-week-1/exercise_shopping_cart_methods/src/ShoppingCartApp.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ShoppingCartJava {
+public class ShoppingCartApp {
     public static void main(String[] args) {
         System.out.println("Welcome to the shopping cart app!");
 
