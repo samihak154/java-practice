c265bd5e4ccf2c96f52d154fd7161090fa2dcc47
samihak154 <samihak154@gmail.com>
2025-06-03 17:17:56 -0500

    Add methods/tasks 5-10

--- java-week-1/exercise_methods/src/Methods.java
+++ java-week-1/exercise_methods/src/Methods.java
@@ -10,6 +10,29 @@ public class Methods {
 
         System.out.println("\nIs 4 even? " + isEven(4));
         System.out.println("Is 7 even? " + isEven(7));
+
+        System.out.println("\n");
+        printMultipleTimes("Hello", 3);
+        printMultipleTimes("Pumba", 2);
+
+        System.out.println("\nMax of 1, 14, 8: " + findMax(1, 14, 8));
+        System.out.println("Max of -5, -25, -2: " + findMax(-5, -25, -2));
+
+        System.out.println("\nFactorial of 7: " + factorial(7));
+        System.out.println("Factorial of 2: " + factorial(2));
+
+        System.out.println("\n");
+        greet("Pumba");
+        greet("Pumba", 9);
+
+        System.out.println("\nVowels in Hello: " + countVowels("hello"));
+        System.out.println("Vowels in World: " + countVowels("world"));
+
+        System.out.println("\n");
+        System.out.println("5 + 5 = " + calculator(5, 5, '+'));
+        System.out.println("6 - 3 = " + calculator(6, 3, '-'));
+        System.out.println("4 * 7 = " + calculator(4, 7, '*'));
+        System.out.println("9 / 3 = " + calculator(9, 3, '/'));
     }
 
     // Task 1
@@ -36,4 +59,70 @@ public class Methods {
         }
     }
 
+    // Task 5
+    public static void printMultipleTimes(String text, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(text);
+        }
+    }
+
+    // Task 6
+    public static int findMax(int a, int b, int c) {
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        return max;
+    }
+
+    // Task 7
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    // Task 8
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+    public static void greet(String name, int age) {
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+    }
+
+    // Task 9
+    public static int countVowels(String text) {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Task 10
+    public static double calculator(int num1, int num2, char operator) {
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("Cannot divide by zero!");
+                }
+                return (double) num1 / num2;
+            default:
+                throw new IllegalArgumentException("Invalid operator!");
+        }
+    }
 }
