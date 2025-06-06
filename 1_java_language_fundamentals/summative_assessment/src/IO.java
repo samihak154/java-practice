8795461a070b4fe5ad30caf1aaa9295a8ea40c2d
samihak154 <samihak154@gmail.com>
2025-06-06 14:26:43 -0500

    Add displayMenu, getMenuChoice, getInputLocker, getInputPin, and displayResult methods

--- 1_java_language_fundamentals/summative_assessment/src/IO.java
+++ 1_java_language_fundamentals/summative_assessment/src/IO.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+
 public class IO {
+    private static Scanner io = new Scanner(System.in);
+
+    public static void displayMenu() {
+        String menu = "What would you like to do next?\n" +
+                " 1. Rent a Locker\n" +
+                " 2. Access a Locker\n" +
+                " 3. Release a Locker\n" +
+                " Any other key to exit.\n" +
+                "==================================\n" +
+                "Enter your choice: ";
+        System.out.print(menu);
+    }
+
+    public static String getMenuChoice() {
+        String inputMenuChoice = io.nextLine();
+        return inputMenuChoice;
+    }
+
+    public static int getInputLocker() {
+        System.out.print("Enter locker number: ");
+        int inputLocker = Integer.parseInt(io.nextLine());
+        return inputLocker;
+    }
+
+    public static String getInputPin() {
+        System.out.print("Enter pin number: ");
+        String inputPin = io.nextLine();
+        return inputPin;
+    }
+
+    public static void displayResult(Result r) {
+        System.out.println(r.getMessage());
+    }
 }
+
