e89bd10b76339a62299e45a2ea5286c7e9ce8cd1
samihak154 <samihak154@gmail.com>
2025-06-06 09:53:27 -0500

    Add constructor and methods: generateRandomPin, rentLocker, and printLockers (used for testing rentLocker)

--- 1_java_language_fundamentals/summative_assessment/src/LockerService.java
+++ 1_java_language_fundamentals/summative_assessment/src/LockerService.java
@@ -1,2 +1,51 @@
+// In charge of managing the lockers: rent, access, release
+import java.util.Random;
+
 public class LockerService {
+    private Locker[] lockers;
+
+    //Constructor
+    public LockerService (int numberofLockers) {
+        lockers = new Locker[numberofLockers];
+
+        for (int i = 0; i < lockers.length; i++) {
+            lockers[i] = new Locker();
+        }
+    }
+
+    private String generateRandomPin() {
+        Random rand = new Random();
+        return String.format("%04d", rand.nextInt(10000));  // 4-digit PIN with leading zeros
+    }
+
+    public Result rentLocker() {
+        // find an available locker, if pin number is null then locker is available
+        for (int i = 0; i < lockers.length; i++) {
+            if (lockers[i].getPinNumber() == null) {
+                // Generate a random pin
+                String pin = generateRandomPin();
+                // Assign the pin to an empty locker
+                lockers[i].setPinNumber(pin);
+                int lockerNumber = i + 1;
+                lockers[i].setLockerNumber(lockerNumber);
+                // Success message
+                return new Result(true, "You have rented a locker, your locker number: " + lockerNumber +
+                                                        "\nAnd here is your pin: " + pin + "\n");
+            }
+        }
+        return new Result(false, "Sorry, all lockers are full!");
+    }
+
+    // for testing rentLocker()
+    public void printLockers() {
+        for (int i = 0; i < lockers.length; i++) {
+            Locker locker = lockers[i];
+            System.out.println(
+                    "Array Index: " + i +
+                            " | Locker Number: " + locker.getLockerNumber() +
+                            " | PIN: " + locker.getPinNumber()
+            );
+        }
+    }
 }
+
