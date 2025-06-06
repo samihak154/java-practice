29de123925bedc268059eef5ae15fd062edeba5e
samihak154 <samihak154@gmail.com>
2025-06-06 09:33:11 -0500

    Add code to handle success/failure outcomes with messages

--- 1_java_language_fundamentals/summative_assessment/src/Result.java
+++ 1_java_language_fundamentals/summative_assessment/src/Result.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Result {
+    private final boolean success;
+    private final String message;
+
+    public Result(boolean success, String message) {
+
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
