a6969c4e241b9507b6dc2dcaf58635ae83941c7c
samihak154 <samihak154@gmail.com>
2025-06-06 14:15:27 -0500

    Declare private fields: lockerNumber and pinNumber and add getters/setters

--- 1_java_language_fundamentals/summative_assessment/src/Locker.java
+++ 1_java_language_fundamentals/summative_assessment/src/Locker.java
@@ -1,2 +1,22 @@
 public class Locker {
+    private int lockerNumber;
+    private String pinNumber;
+
+    // Getters
+    public int getLockerNumber() {
+        return lockerNumber;
+    }
+
+    public String getPinNumber() {
+        return pinNumber;
+    }
+
+    //Setters
+    public void setLockerNumber(int lockerNumber) {
+        this.lockerNumber = lockerNumber;
+    }
+
+    public void setPinNumber(String pinNumber) {
+        this.pinNumber = pinNumber;
+    }
 }
