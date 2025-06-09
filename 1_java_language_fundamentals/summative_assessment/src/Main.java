46f8b6f7da1d249327313bb6675a88d0bc012d1a
samihak154 <samihak154@gmail.com>
2025-06-09 01:13:33 -0500

    Remove planning comments

--- 1_java_language_fundamentals/summative_assessment/src/Main.java
+++ 1_java_language_fundamentals/summative_assessment/src/Main.java
@@ -1,38 +1,12 @@
-// App workflow
-
 public class Main {
     public static void main(String[] args) {
-        // 1. Welcome user
-        // 2. Display menu
-        // 3. Get menu choice
-        // 4. Based on choice do task, if choice is:
-            // a) Rent:
-                // Get next available locker
-                // Get random pin number
-                // Store locker number and pin number in arrays
-                // Show thank you message and instructions
-            // b) Access:
-                // Prompt user for locker number and PIN
-                // Validate input against stored data
-                // Grant access if match is found, else show error
-            // c) Release:
-                // Prompt for locker number and PIN
-                // Confirm action: “Are you sure?” (Yes/No)
-                // Clear data from array if confirmed
-                // Show error for incorrect PIN or locker
-            // d) Quit:
-                // User can quit main menu anytime by pressing any key
-                // other than the choices (1, 2, or 3)
-        // Loop back to main menu after doing tasks and showing messages
-
-
         LockerService service = new LockerService(10);
         boolean runningMenu = true;
 
         System.out.println("=== Welcome to Locker Manager! ===");
 
         while (runningMenu) {
-            IO.displayMenu();
+            IO.displayMenu(service.lockersAvailable());
             String choice = IO.getMenuChoice();
 
             switch (choice) {
