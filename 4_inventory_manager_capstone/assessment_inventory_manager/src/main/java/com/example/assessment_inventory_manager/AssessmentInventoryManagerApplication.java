cc09f3dc14fa58390c92a4c44a81f64164acb2ed
samihak154 <samihak154@gmail.com>
2025-07-09 16:21:21 -0500

    Add empty project packages

---
+++ 4_inventory_manager_capstone/assessment_inventory_manager/src/main/java/com/example/assessment_inventory_manager/AssessmentInventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.assessment_inventory_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentInventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentInventoryManagerApplication.class, args);
+	}
+
+}
---
+++ 4_inventory_manager_capstone/assessment_inventory_manager/src/test/java/com/example/assessment_inventory_manager/AssessmentInventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assessment_inventory_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentInventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
